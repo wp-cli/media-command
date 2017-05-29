861babaa2e85151b9385deab82a675b8a46d0f05
gitlost <burmartke@gmail.com>
2017-05-29 10:29:58 +0100

    Split cache by WP_VERSION also.

--- bin/install-imagick.sh
+++ bin/install-imagick.sh
@@ -13,18 +13,18 @@ install_imagemagick() {
 	rm "ImageMagick-$IMAGEMAGICK_VERSION.tar.xz"
 	cd "ImageMagick-$IMAGEMAGICK_VERSION"
 
-	./configure --prefix="$HOME/opt/$TRAVIS_PHP_VERSION" --with-perl=no
+	./configure --prefix="$HOME/opt/$WP_VERSION/$TRAVIS_PHP_VERSION" --with-perl=no
 	make
 	make install
 
 	# Don't need doc - saves around 24M.
-	if [[ -d "$HOME/opt/$TRAVIS_PHP_VERSION/share/doc" ]]; then rm -rf "$HOME/opt/$TRAVIS_PHP_VERSION/share/doc"; fi
+	if [[ -d "$HOME/opt/$WP_VERSION/$TRAVIS_PHP_VERSION/share/doc" ]]; then rm -rf "$HOME/opt/$WP_VERSION/$TRAVIS_PHP_VERSION/share/doc"; fi
 
 	cd "$TRAVIS_BUILD_DIR"
 }
 
 # Install ImageMagick if the current version isn't up to date
-PATH="$HOME/opt/$TRAVIS_PHP_VERSION/bin:$PATH" identify -version | grep "$IMAGEMAGICK_VERSION" || install_imagemagick
+PATH="$HOME/opt/$WP_VERSION/$TRAVIS_PHP_VERSION/bin:$PATH" identify -version | grep "$IMAGEMAGICK_VERSION" || install_imagemagick
 
 # Install Imagick for PHP
-echo "$HOME/opt/$TRAVIS_PHP_VERSION" | pecl install -f "imagick-$IMAGICK_VERSION"
+echo "$HOME/opt/$WP_VERSION/$TRAVIS_PHP_VERSION" | pecl install -f "imagick-$IMAGICK_VERSION"
