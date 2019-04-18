aef39206f4d471471429c148a09669715fe82569
Thrijith Thankachan <thrijith13@gmail.com>
2019-04-18 12:59:55 +0530

    PHPCS: Fix up errors in media-command.php

--- media-command.php
+++ media-command.php
@@ -4,16 +4,22 @@ if ( ! class_exists( 'WP_CLI' ) ) {
 	return;
 }
 
-$autoload = dirname( __FILE__ ) . '/vendor/autoload.php';
-if ( file_exists( $autoload ) ) {
-	require_once $autoload;
+$wpcli_media_autoloader = dirname( __FILE__ ) . '/vendor/autoload.php';
+if ( file_exists( $wpcli_media_autoloader ) ) {
+	require_once $wpcli_media_autoloader;
 }
 
-WP_CLI::add_command( 'media', 'Media_Command', array(
-	'before_invoke' => function () {
-		if ( !wp_image_editor_supports() ) {
-			WP_CLI::error( 'No support for generating images found. ' .
-				'Please install the Imagick or GD PHP extensions.' );
-		}
-	}
-) );
+WP_CLI::add_command(
+	'media',
+	'Media_Command',
+	array(
+		'before_invoke' => function () {
+			if ( ! wp_image_editor_supports() ) {
+				WP_CLI::error(
+					'No support for generating images found. ' .
+					'Please install the Imagick or GD PHP extensions.'
+				);
+			}
+		},
+	)
+);
